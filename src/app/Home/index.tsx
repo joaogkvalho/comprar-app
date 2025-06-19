056fa346cbe77b89179ca8ed5f10a51fdc837f5c
joaogkvalho <joaogkvalho@gmail.com>
2025-06-19 12:59:03 -0300

    features added

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,14 +1,59 @@
-import { StatusBar, Text, View } from "react-native";
+import { Button } from "@/components/Button";
+import { Filter } from "@/components/Filter";
+import { Input } from "@/components/Input";
+import { Item } from "@/components/Item";
+
+import { FilterStatus } from "@/types/FilterStatus";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
+
+const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE]
+const ITEMS = [
+  { id: "1", status: FilterStatus.DONE, description: "1 pacote de café" },
+  { id: "2", status: FilterStatus.PENDING, description: "3 pacotes de macarrão" },
+  { id: "3", status: FilterStatus.PENDING, description: "3 cebolas" },
+]
 
 export function Home() {
   return (
-    <>
-      <View style={styles.container}>
-        <Text style={styles.text}>Hello, World</Text>
+    <View style={styles.container}>
+      <Image 
+        source={require("@/assets/logo.png")} 
+        style={styles.logo} 
+      />
+
+     <View style={styles.form}>
+      <Input placeholder="O que você precisa comprar?" />
+      <Button title="Entrar" />
+     </View>
+
+     <View style={styles.content}>
+      <View style={styles.header}>
+        {FILTER_STATUS.map((status) => (
+          <Filter key={status} status={status} isActive />
+        ))}
+
+        <TouchableOpacity style={styles.clearButton}>
+          <Text style={styles.clearText}>Limpar</Text>
+        </TouchableOpacity>
       </View>
 
-      <StatusBar barStyle="light-content" />
-    </>
+      <FlatList 
+        data={ITEMS}
+        keyExtractor={item => item.id}
+        showsVerticalScrollIndicator={false}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+        ListEmptyComponent={() => <Text style={styles.empty}>Nenhum item aqui.</Text>}
+        contentContainerStyle={styles.listContent}
+        renderItem={({ item }) => (
+          <Item 
+            data={item}
+            onStatus={() => console.log("trocar status")}
+            onRemove={() => console.log("remover")}
+          />
+        )}
+      />
+     </View>
+    </View>
   )
 }
---
+++ src/components/Item/index.tsx
@@ -0,0 +1,35 @@
+import { Trash2 } from "lucide-react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+
+import { FilterStatus } from "@/types/FilterStatus";
+import { StatusIcon } from "../StatusIcon";
+import { styles } from "./styles";
+
+type ItemData = {
+    status: FilterStatus
+    description: string
+}
+
+type Props = {
+    data: ItemData
+    onRemove: () => void
+    onStatus: () => void
+}
+
+export function Item({ data, onStatus, onRemove }: Props) {
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity activeOpacity={0.8} onPress={onStatus}>
+                <StatusIcon status={data.status} />
+            </TouchableOpacity>
+
+            <Text style={styles.description}>
+                {data.description}
+            </Text>
+
+            <TouchableOpacity onPress={onRemove}>
+                <Trash2 size={18} color="#828282" />
+            </TouchableOpacity>
+        </View>
+    )
+}
